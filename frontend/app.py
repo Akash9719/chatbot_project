49f761256bf29281c05b693d34b014e6d1a9faf9
Akash9719 <78297767+Akash9719@users.noreply.github.com>
2026-06-10 12:34:34 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,5 @@
 import os
 import streamlit as st
-import numpy as np
 from groq import Groq
 
 # Google Sheets
@@ -19,14 +18,21 @@ st.title("💬 Rishikirti AI Assistant")
 # Setup
 # -----------------------
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
 @st.cache_resource
 def load_knowledge():
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, "knowledge.txt")
+
+    if not os.path.exists(file_path):
+        st.error(f"❌ knowledge.txt not found at: {file_path}")
+        st.stop()
+
     with open(file_path, "r", encoding="utf-8") as f:
         return f.read()
 
 knowledge = load_knowledge()
+
 def retrieve(query):
     return knowledge
 
